942c18f8cca642bdd1e35ee20aede2ca949efda4
opturtio <86497253+opturtio@users.noreply.github.com>
2024-11-11 21:09:03 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS messages (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP,
     msg TEXT
-)
+);
 
 CREATE TABLE IF NOT EXISTS peers (
     id SERIAL PRIMARY KEY,
     user_id TEXT,
     name TEXT,
     ip TEXT
-)
+);
