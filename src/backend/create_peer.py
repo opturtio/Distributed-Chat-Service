be2e1f77c85ab004525c1c796f73a0858b80e4c1
Sohvi Puska <sohvip1@gmail.com>
2024-11-23 12:31:33 +0200

    use logger in create_peer

--- src/backend/create_peer.py
+++ src/backend/create_peer.py
@@ -1,14 +1,11 @@
-import logging
 from backend.peer import Peer
 
 def create_peer():
     """Initializes the peer"""
     
-    # logging.basicConfig(level=logging.INFO)
-    # logger = logging.getLogger(__name__)
-    
     peer = Peer("localhost", 5010)
     peer.start()
-    # logger.info(f"Peer started at {peer.host}:{peer.port}")
+    peer.setup_logger()
+    peer.logger.info(f"Peer started at {peer.host}:{peer.port}")
 
     return peer
