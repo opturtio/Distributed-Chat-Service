c3ebcd035b44c526e95b2a97ae21ac7dd85dc108
opturtio <olli-pekka.turtio@helsinki.fi>
2024-11-24 21:26:22 +0200

    Revert "use logger in create_peer"
    This reverts commit be2e1f77c85ab004525c1c796f73a0858b80e4c1.

--- src/backend/create_peer.py
+++ src/backend/create_peer.py
@@ -1,11 +1,14 @@
+import logging
 from backend.peer import Peer
 
 def create_peer():
     """Initializes the peer"""
     
+    # logging.basicConfig(level=logging.INFO)
+    # logger = logging.getLogger(__name__)
+    
     peer = Peer("localhost", 5010)
     peer.start()
-    peer.setup_logger()
-    peer.logger.info(f"Peer started at {peer.host}:{peer.port}")
+    # logger.info(f"Peer started at {peer.host}:{peer.port}")
 
     return peer
