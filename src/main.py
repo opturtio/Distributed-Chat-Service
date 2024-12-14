2ca04055c6e8d2887e14bc26e1b5a17dc33b9000
Aarni <aarni.miikkulainen@helsinki.fi>
2024-12-14 14:10:27 +0200

    Add logging for 'peer_port_own' environment variable

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ load_dotenv()
 print("Current working directory:", os.getcwd())
 print("peer_host:", os.getenv("peer_host"))
 print("peer_port:", os.getenv("peer_port"))
+print("peer_port_own:", os.getenv("peer_port_own"))
 
 def console_menu(peer):
     """Provides a console-based menu for user interaction.
