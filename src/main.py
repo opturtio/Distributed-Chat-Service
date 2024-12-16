1691b9356bab56e0b054fc137b73f1d6dd396446
Aarni <aarni.miikkulainen@helsinki.fi>
2024-12-16 20:36:20 +0200

    add own ip into .env

--- src/main.py
+++ src/main.py
@@ -108,6 +108,6 @@ def view_messages_cli(messages):
     return messages
 
 if __name__ == "__main__":
-    peer = Peer("0.0.0.0", os.getenv("peer_port_own"))
+    peer = Peer(os.getenv("peer_host_own"), os.getenv("peer_port_own"))
     peer.start()
     console_menu(peer)
